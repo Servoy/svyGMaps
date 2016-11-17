b001eacf2e0661aadda76c2c4d6321c1ed4048a2
Tuan Nguyen <tuanway@gmail.com>
2016-11-17 15:24:08 -0600

    Update example solution.

--- googleMapsExample/forms/responsiveLayout.js
+++ googleMapsExample/forms/responsiveLayout.js
@@ -1,4 +1,11 @@
 /**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"323157CC-F585-41D3-9B60-5225DB860BB9"}
+ */
+var logo = 'media:///logo.png';
+
+/**
  * @public 
  * @properties={typeid:24,uuid:"94BD5635-1421-446A-885C-FD0D1EE3AE46"}
  */
--- googleMapsExample/maps.js
+++ googleMapsExample/maps.js
@@ -10,7 +10,7 @@ var addr = '';
  * TODO: add error checking for invalid keys
  * @properties={typeid:35,uuid:"4D8F25D9-51FA-4B66-B8C5-4B2B93DDF749"}
  */
-var apiKey = 'Your google map api key'
+var apiKey = 'Your Google API Key'
 /**
  *
  * @param {JSEvent} event
